810c35e5fd6f55abf0937a0d1310bdfeda034d76
Alex Tsantilis <alex.tsantilis@gmail.com>
2019-01-07 08:50:17 -0500

    added code files

---
+++ ibm_8b_10b_encoder.ads
@@ -0,0 +1,13 @@
+package IBM_8b_10b_Encoder is
+
+    type Byte is mod 2**8;
+    type Ten_Bits is mod 2**10;
+    type Running_Disp is (Neg_1, Pos_1);
+
+    procedure Encode(B: in Byte; T: out Ten_Bits; RD: in out Running_Disp);
+
+    --To verify that Running Disparity is being preserved, use this version
+    procedure Decode(T: in Ten_Bits; B: out Byte; RD: in out Running_Disp);
+
+    procedure Decode(T: in Ten_Bits; B: out Byte);
+end IBM_8b_10b_Encoder;
